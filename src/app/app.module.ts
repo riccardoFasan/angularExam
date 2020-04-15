eb0e462b5cfc2de23d4aa66b3c8c3ed4a4aa6a26
Riccardo Fasan <57989127+richardTheArless@users.noreply.github.com>
2020-04-15 09:41:47 +0200

    default html content removed. Routing prepared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+// Angular Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+//Components
 import { AppComponent } from './app.component';
+
+// Routes
+const appRoutes : Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
